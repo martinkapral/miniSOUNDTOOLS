02158a20af87cf08868391f81389766a40cde1ab
martinkapral <147430307+martinkapral@users.noreply.github.com>
2024-01-23 14:37:47 +0100

    Update app.js layout update1

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//
+//
+// REVERB CALCULATOR
+//
+//
+
 let bpmReverb = document.getElementById("bpm-reverb");
 const convertBtn = document.getElementById("convert-btn");
 
@@ -62,5 +68,107 @@ function calculateReverb() {
     preTAnumber.innerHTML = `${(preTA / bpmReverb.value).toFixed(2)} ms`;
     decayTAnumber.innerHTML = `${(decayTA / bpmReverb.value).toFixed(2)} ms`;
     totalTAnumber.innerHTML = `${(totalTA / bpmReverb.value).toFixed(2)} ms`;
+  }
+}
+
+//
+//
+// DELAY CALCULATOR
+//
+//
+
+// 1/1 CONVERSION
+
+let notes11 = document.getElementById("notes11");
+let dotted11 = document.getElementById("dotted11");
+let triplets11 = document.getElementById("triplets11");
+
+let notes = 240000;
+let dotted = 360000;
+let triplets = 160000;
+
+// 1/2 CONVERSION
+
+let notes12 = document.getElementById("notes12");
+let dotted12 = document.getElementById("dotted12");
+let triplets12 = document.getElementById("triplets12");
+
+// 1/4 CONVERSION
+
+let notes14 = document.getElementById("notes14");
+let dotted14 = document.getElementById("dotted14");
+let triplets14 = document.getElementById("triplets14");
+
+// 1/8 CONVERSION
+
+let notes18 = document.getElementById("notes18");
+let dotted18 = document.getElementById("dotted18");
+let triplets18 = document.getElementById("triplets18");
+
+// 1/16 CONVERSION
+
+let notes116 = document.getElementById("notes116");
+let dotted116 = document.getElementById("dotted116");
+let triplets116 = document.getElementById("triplets116");
+
+// 1/32 CONVERSION
+
+let notes132 = document.getElementById("notes132");
+let dotted132 = document.getElementById("dotted132");
+let triplets132 = document.getElementById("triplets132");
+
+// 1/64 CONVERSION
+
+let notes164 = document.getElementById("notes164");
+let dotted164 = document.getElementById("dotted164");
+let triplets164 = document.getElementById("triplets164");
+
+// 1/128 CONVERSION
+
+let notes1128 = document.getElementById("notes1128");
+let dotted1128 = document.getElementById("dotted1128");
+let triplets1128 = document.getElementById("triplets1128");
+
+function calculateDelay() {
+  if (bpmReverb.value > 0) {
+    notes11.innerHTML = `${(notes / bpmReverb.value).toFixed(2)} ms`;
+    dotted11.innerHTML = `${(dotted / bpmReverb.value).toFixed(2)} ms`;
+    triplets11.innerHTML = `${(triplets / bpmReverb.value).toFixed(2)} ms`;
+
+    notes12.innerHTML = `${(notes / bpmReverb.value / 2).toFixed(2)} ms`;
+    dotted12.innerHTML = `${(dotted / bpmReverb.value / 2).toFixed(2)} ms`;
+    triplets12.innerHTML = `${(triplets / bpmReverb.value / 2).toFixed(2)} ms`;
+
+    notes14.innerHTML = `${(notes / bpmReverb.value / 4).toFixed(2)} ms`;
+    dotted14.innerHTML = `${(dotted / bpmReverb.value / 4).toFixed(2)} ms`;
+    triplets14.innerHTML = `${(triplets / bpmReverb.value / 4).toFixed(2)} ms`;
+
+    notes18.innerHTML = `${(notes / bpmReverb.value / 8).toFixed(2)} ms`;
+    dotted18.innerHTML = `${(dotted / bpmReverb.value / 8).toFixed(2)} ms`;
+    triplets18.innerHTML = `${(triplets / bpmReverb.value / 8).toFixed(2)} ms`;
+
+    notes116.innerHTML = `${(notes / bpmReverb.value / 16).toFixed(2)} ms`;
+    dotted116.innerHTML = `${(dotted / bpmReverb.value / 16).toFixed(2)} ms`;
+    triplets116.innerHTML = `${(triplets / bpmReverb.value / 16).toFixed(
+      2
+    )} ms`;
+
+    notes132.innerHTML = `${(notes / bpmReverb.value / 32).toFixed(2)} ms`;
+    dotted132.innerHTML = `${(dotted / bpmReverb.value / 32).toFixed(2)} ms`;
+    triplets132.innerHTML = `${(triplets / bpmReverb.value / 32).toFixed(
+      2
+    )} ms`;
+
+    notes164.innerHTML = `${(notes / bpmReverb.value / 64).toFixed(2)} ms`;
+    dotted164.innerHTML = `${(dotted / bpmReverb.value / 64).toFixed(2)} ms`;
+    triplets164.innerHTML = `${(triplets / bpmReverb.value / 64).toFixed(
+      2
+    )} ms`;
+
+    notes1128.innerHTML = `${(notes / bpmReverb.value / 128).toFixed(2)} ms`;
+    dotted1128.innerHTML = `${(dotted / bpmReverb.value / 128).toFixed(2)} ms`;
+    triplets1128.innerHTML = `${(triplets / bpmReverb.value / 128).toFixed(
+      2
+    )} ms`;
   }
 }
